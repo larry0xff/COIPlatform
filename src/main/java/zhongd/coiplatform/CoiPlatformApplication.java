02c53864eb1b022e45dac287eedf3b2c34472597
zhongd <xzdGet2y@163.com>
2017-11-02 16:22:35 +0800

    init proj

---
+++ src/main/java/zhongd/coiplatform/CoiPlatformApplication.java
@@ -0,0 +1,12 @@
+package zhongd.coiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoiPlatformApplication.class, args);
+	}
+}
