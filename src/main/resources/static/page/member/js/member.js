4763aa26789f99ca90bbba4dacbe75b85b99d374
Mango <xzdGet2y@163.com>
2017-12-05 23:37:38 +0800

    成员列表页面开始编写

--- src/main/resources/static/page/member/js/member.js
+++ src/main/resources/static/page/member/js/member.js
@@ -2,16 +2,20 @@ var memberApp = angular.module('memberApp', []);
 
 memberApp.controller('listCtrl', ['$scope', '$http', function($scope, $http){
     $scope.list = function(){
-        $http.post(contextPath + '/member/list', $scope.param).then(function(response){
+        $http.post(contextPath + '/member/list' + $scope.param).then(function(response){
             var data = response.data;
             if(data.returnCode != 200){
-
+                Materialize.toast(data.msg, 2000);
+            }else{
+                $scope.memberList = data.data.list;
+                $scope.memberCount = data.data.count;
             }
         });
     };
     $scope.init = function(){
-
-
+        $scope.param = '?page=1&pageSize=20';
+        $scope.list();
     };
+    $scope.init();
 
 }]);
