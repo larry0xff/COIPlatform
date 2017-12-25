d21b65d6441084369b2e25dbfa01bba3d6b8898b
xiezhongdong <xiezhongdong@leyaoyao.org>
2017-12-25 18:38:46 +0800

    征集部分

--- src/main/resources/static/page/advice/js/advice.js
+++ src/main/resources/static/page/advice/js/advice.js
@@ -28,9 +28,45 @@ adviceApp.controller('adviceDeadListCtrl', ['$scope', '$http', function ($scope,
             }
         });
     };
+    $scope.handle = function(filename){
+        var formData = {
+            resultAttachmentUrl: filename,
+            igAdviceCollectionId: $scope.igAdviceCollectionId
+        };
+        $http({
+            url: contextPath + '/adviceCollection/handle',
+            data: formData,
+            method: 'POST'
+        }).then(function(response){
+            var data = response.data;
+            if(data.returnCode != 200){
+                Materialize.toast(data.msg, 2000);
+            }else{
+
+            }
+        });
+    };
+    $scope.upload = function(){
+        var file = document.getElementById('uploadfile').files[0];
+        var fd = new FormData();
+        fd.append('file', file);
+        fd.append('type', 3);
+        $http({
+            url: contextPath + '/file/upload',
+            method: 'POST',
+            data: fd,
+            headers: {'Content-Type':undefined}
+        }).then(function(response){
+            var data = response.data;
+            if(data.returnCode != 200){
+                Materialize.toast(data.msg, 2000);
+            }else{
+                $scope.handle(data.data);
+            }
+        });
+    };
     var init = function(){
         $scope.contextPath = contextPath;
         getList();
     }();
-
 }]);
